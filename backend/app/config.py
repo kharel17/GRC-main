b422c2ecfdc7c9dc18ca9c9816365d61074c2be4
kharel17 <kharel.anush17@gmail.com>
2026-03-08 23:54:36 +0545

    fix: allow '*' in BACKEND_CORS_ORIGINS

--- backend/app/config.py
+++ backend/app/config.py
@@ -38,16 +38,17 @@ class Settings(BaseSettings):
         return uri
 
     # CORS
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    BACKEND_CORS_ORIGINS: List[Union[AnyHttpUrl, str]] = []
 
     @field_validator("BACKEND_CORS_ORIGINS", mode="before")
     @classmethod
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
         if isinstance(v, str) and not v.startswith("["):
             return [i.strip() for i in v.split(",")]
-        elif isinstance(v, (list, str)):
-            return v
-        raise ValueError(v)
+        elif isinstance(v, str) and v.startswith("["):
+            import json
+            return json.loads(v)
+        return v
 
     # FILE STORAGE
     FILE_STORAGE_BACKEND: str = "local"  # "local" or "s3"

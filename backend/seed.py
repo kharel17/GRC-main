9bd9fa1a978744c5e17b2475ac7742bf3d18d59a
1Gab1 <godacnology@gmail.com>
2026-09-03 11:48:14 +0545

    Docker changes

--- backend/seed.py
+++ backend/seed.py
@@ -23,7 +23,6 @@ def hash_password(password: str) -> str:
 # Create tables
 async def init_db():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
 async def seed_data():
